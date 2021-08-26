7f02afa4b082e8a54f5e4fa141e40c3049d2f030
Daniel Moldovan <dmoldovan.101@gmail.com>
2021-08-26 21:33:35 +0200

    Solved the Column selection.

--- scriptCSV.ps1
+++ scriptCSV.ps1
@@ -1,34 +1,11 @@
 #Windows - Powershell in CLI / Mac - pwsh
 #Windows PS - .\scriptCSV.ps1
-$Headers = @("ErrorCode") # Add all other desired headers here
+
 # Import the CSV
-$path = "newfileSimple.csv"
-$data = Import-CSV $path
-#Write-Output $data[0]
+$path = "17253.csv"
 
-#Write-Output $data[1]
+$A = Import-Csv $path -Delimiter ';' -Header '1', '2', '3', '4'
 
-#Write-Output $data[2]
+$A | Where-Object -Property 4 -Like '65'
 
-# Take the first row and get its columns
-$columns = $data[0].psobject.properties.name
-#Write-Output $columns[0]
-#Write-Output $columns[1]
-#Write-Output $columns[2]
-
-#Write-Output $data[0]
-Write-Output $data[1]
-#Write-Output $data[2]
-
-#$Column1 = $columns | Where-Object {$data[0].$_ -match "66"}
-
-#Write-Output $Column1
-
-#$data[1] | Select-Object $Column1 | Export-CSV outfile.csv -notypeinformation -delimiter ';'
-
-#"$((Get-Date).ToShortDateString())" # << Header for todays date e.g. 19/06/2018
-#Import-Csv $path | Select $Headers | Export-Csv -Path "C:\Foofolder\csv\ExportFile.csv" -Force -NoTypeInformation
-Import-Csv $path | Select $Headers | Export-Csv outfile.csv -notypeinformation -delimiter ';'
-
-#Import-Csv newfile.csv| sort ErrorCode | export-CSV outfile.csv
-#Import-Csv newfileSimple.csv| Select $Headers | Export-Csv outfile.csv
+$A.Count
